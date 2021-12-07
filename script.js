c293dfa6ae84febc5c09d29e2c4a3e808b92517b
ayonsa60 <ayonsa60@gmail.com>
2021-12-06 21:01:19 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var city = function() {
+    api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey};
+}
+
+
+
+function userInput(locationSearch){
+    var element = document.getElementById('locationSearch');
+
+}
+
