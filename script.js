de5abe5e0270f1d54c929af45ffe497ab610e9da
ayonsa60 <ayonsa60@gmail.com>
2021-12-07 14:11:28 -0600

    added global variables and DOM references

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
 
-var city = function() {
-    api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey};
+let previousSearch = [];
+let apiUrl = 'https://api.openweathermap.org';
+let apiKey = '1d62e0d5f6728111b13e38cdd76d1f28';
+
+let locationSearch = document.querySelector('#locationSearch');
+let searchField = document.querySelector('#searchField');
+let previousSearchField= document.querySelector('#previousSearchField')
+
+function renderPreviousSearchField() {
+    previousSearchField.innerHTML = '';
 }
 
+function getPreviousSearch() {
+    let searchHistory = localStorage.getItem('previousSearch');
+    if(searchHistory) {
+        previousSearch = JSON.parse(searchHistory);
+    }
+    renderPreviousSearchField();
+}
 
-
-function userInput(locationSearch){
-    var element = document.getElementById('locationSearch');
+function handlePreviousSearchClick() {
 
 }
 
+function handleLocationSearchSubmit() {
+    if(!searchField.value) {
+
+    }
+}
+getPreviousSearch();
+locationSearch.addEventListener ('submit', handleLocationSearchSubmit);
+previousSearchField.addEventListener('click', handlePreviousSearchClick);
